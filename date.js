4663b844ccb70bb9edd8b7d71d5a9e117b552312
mokbhai <mokshitjain18@gmail.com>
2024-09-09 00:05:56 +0530

    Book

--- date.js
+++
@@ -1,6 +0,0 @@
-const generateIssueDate = () => {
-  const currentDate = new Date();
-  return currentDate.toISOString(); // Returns the date in ISO 8601 format
-};
-
-console.log(generateIssueDate()); // Example output: "2024-09-08T02:12:00.000Z"
